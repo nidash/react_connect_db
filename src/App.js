2edf95b830c07a5ebcba62ac3853cba8bae9ad79
Nidash <nidash@gmail.com>
2023-09-21 21:58:47 +0100

    Adding isLoading

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setisLoading(true);
     const response = await fetch('https://swapi.dev/api/films');
     const data = await response.json();
     const transformedMovies = data.results.map(movie => {
@@ -18,6 +20,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setisLoading(false);
   }
 
   return (
@@ -26,7 +29,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
